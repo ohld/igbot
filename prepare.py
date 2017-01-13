e644f1dff199678c08f0898a7b266b841753b110
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-13 22:41:04 +0300

    add preparation script
    it creates secret.txt file with login and password which will be used
    later

---
+++ prepare.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import os, sys
+
+def check_secret():
+    while True:
+        if os.path.exists("secret.txt"):
+            with open("secret.txt", "r") as f:
+                try:
+                    login = f.readline().strip()
+                    password = f.readline().strip()
+                    if len(login) < 4 or len(password) < 8:
+                        print ("Data in 'secret.txt' file is invalid. We will delete it and try again.")
+                        os.remove("secret.txt")
+                    else:
+                        return True
+                except:
+                    print ("Your file is broken. We will delete it and try again.")
+                    os.remove("secret.txt")
+        else:
+            with open("secret.txt", "w") as f:
+                print ("We need to create a text file 'secret.txt' where we will store your login and password from Instagram.")
+                print ("Don't worry. It will be stored locally.")
+                print ("Enter your login: ")
+                f.write(str(sys.stdin.readline()))
+                print ("Enter your password: ")
+                f.write(str(sys.stdin.readline()))
+
+
+if __name__ == "__main__":
+    check_secret()
