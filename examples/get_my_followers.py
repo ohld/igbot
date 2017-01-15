bbfa8a71c5b5fa0efe354c72445795c39f2be399
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-15 22:26:56 +0300

    style correction

--- examples/get_my_followers.py
+++ examples/get_my_followers.py
@@ -7,20 +7,20 @@ sys.path.append(os.path.join(sys.path[0],'../src'))
 from core import Instacore
 from prepare import get_credentials
 
-def get_my_followers():
-    """Gets followers of logged user. Stores locally in *.csv."""
-    login, password = get_credentials()
-    core = Instacore(login, password)
-
-    followers = core.get_followers(login)
+def get_followers(username):
+    """Gets followers of username. Stores locally in *.csv."""
+    followers = core.get_followers(username)
     df = pd.DataFrame(followers)
 
     now = datetime.datetime.now()
-    df.to_csv("followers_%s_%s.csv"%(login, now.strftime("%Y-%m-%d %H:%M")),
+    df.to_csv("followers_%s_%s.csv"%(username, now.strftime("%Y-%m-%d %H:%M")),
               index=False, encoding="utf8")
 
     core.logout()
     return True
 
 if __name__ == "__main__":
-    get_my_followers()
+    login, password = get_credentials()
+    core = Instacore(login, password)
+    get_followers(login)
+    core.logout()
