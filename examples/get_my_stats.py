abc7810bbf063834c35d0b3263704e9bcd4270ae
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-15 22:18:07 +0300

    add stats collector example
    this script will save your stats into «stats_username.csv» so you can
    plot them and analyze your account

---
+++ examples/get_my_stats.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import pandas as pd
+import datetime, time
+import sys, os
+
+sys.path.append(os.path.join(sys.path[0],'../src'))
+from core import Instacore
+from prepare import get_credentials
+
+def save_stats(username):
+    """ Saves the number of medias, followers and followed by
+        for future analysis."""
+
+    # get info
+    info = core.get_profile_info(username)
+    stats = {
+        info["date"]: {
+                "media": info["media"],
+                "follows": info["follows"],
+                "followed_by": info["followed_by"]
+        }
+    }
+
+    # save
+    path_to_csv = "stats_%s.csv"%(username)
+    df = pd.DataFrame.from_dict(stats).T
+    if os.path.exists(path_to_csv):
+        df.to_csv(path_to_csv, mode='a', header=False)
+    else:
+        df.to_csv(path_to_csv, header=True)
+
+    return True
+
+if __name__ == "__main__":
+    login, password = get_credentials()
+    core = Instacore(login, password)
+    while True:
+        save_stats(login)
+        time.sleep(1 * 60 * 60)
+    core.logout()
