79287a92049b56680f963e9644334e0a995cbbdc
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-17 19:36:01 +0300

    add files to pip package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'instabot',
+  packages = ['instabot'],
+  version = '0.1',
+  description = 'Cool Instagram bot scripts and API python wrapper.',
+  author = 'Daniil Okhlopkov',
+  author_email = 'ohld@icloud.com',
+  url = 'https://github.com/ohld/instabot',
+  download_url = 'https://github.com/ohld/instabot/tarball/0.1', # I'll explain this in a second
+  keywords = ['instagram', 'bot', 'api'], # arbitrary keywords
+  classifiers = [],
+)
