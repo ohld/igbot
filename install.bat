f2400642aeaeaba95720605a48d9df72ea0a9794
Killerherts <killerherts@gmail.com>
2017-01-19 19:50:32 -0600

    Added windows batch for easy install
    Windows batch file that installs instabot then install requirements

--- install.bat
+++ install.bat
@@ -6,6 +6,7 @@ path c:\Program Files\Git\cmd;%PATH%
 path C:\Python27;%PATH%
 path C:\Python27\Scripts;%PATH%
 
- pip install -r requirements.txt
- print "Please close this window requirements are now installed"
- pause 
+pip install instabot
+pip install -r requirements.txt
+print "Congradulations You now install with requiments and instabot"
+pause 
