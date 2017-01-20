3091b6694509685c2839172dc029edcca8f9fdc6
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-20 12:41:22 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'instabot',
   packages = ['instabot'],
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'Cool Instagram bot scripts and API python wrapper.',
   author = 'Daniil Okhlopkov',
   author_email = 'ohld@icloud.com',
   url = 'https://github.com/ohld/instabot',
-  download_url = 'https://github.com/ohld/instabot/tarball/0.1.3', # I'll explain this in a second
+  download_url = 'https://github.com/ohld/instabot/tarball/0.1.4', # I'll explain this in a second
   keywords = ['instagram', 'bot', 'api'], # arbitrary keywords
   classifiers = [],
   install_requires=['tqdm', 'pandas'],
