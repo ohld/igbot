3aa0d390bf717f00d3fdcbdf9938cb2471dd4a69
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-20 17:31:14 +0300

    fix to have proper USER_AGENT string

--- instabot/config.py
+++ instabot/config.py
@@ -2,8 +2,8 @@
 Configuration file of the project
 """
 
-USER_AGENT = """Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36
-(KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36"""
+USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 "
+              "(KHTML, like Gecko) Chrome/48.0.2564.103 Safari/537.36")
 ACCEPT_LANGUAGE = "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4"
 BASE_COOKIE = {'sessionid': '', 'mid': '', 'ig_pr': '1', 'ig_vw': '1920',
                'csrftoken': '', 's_network': '', 'ds_user_id': ''}
