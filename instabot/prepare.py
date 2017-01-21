5dbac8c206ecada17ed965563c9b065dd3cc798e
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-21 20:10:18 +0300

    add delete_credentials function

--- instabot/prepare.py
+++ instabot/prepare.py
@@ -2,12 +2,13 @@
 import os
 import sys
 
+SECRET_FILE = "secret.txt"
 
 def get_credentials():
     "Returns login and password stored in secret.txt"
     while not check_secret():
         pass
-    with open("secret.txt", "r") as f:
+    with open(SECRET_FILE, "r") as f:
         login = f.readline().strip()
         password = f.readline().strip()
     return login, password
@@ -15,23 +16,23 @@ def get_credentials():
 
 def check_secret():
     while True:
-        if os.path.exists("secret.txt"):
-            with open("secret.txt", "r") as f:
+        if os.path.exists(SECRET_FILE):
+            with open(SECRET_FILE, "r") as f:
                 try:
                     login = f.readline().strip()
                     password = f.readline().strip()
                     if len(login) < 4 or len(password) < 8:
                         print("Data in 'secret.txt' file is invalid. "
                               "We will delete it and try again.")
-                        os.remove("secret.txt")
+                        os.remove(SECRET_FILE)
                     else:
                         return True
                 except IOError:
                     print("Your file is broken. We will delete it "
                           "and try again.")
-                    os.remove("secret.txt")
+                    os.remove(SECRET_FILE)
         else:
-            with open("secret.txt", "w") as f:
+            with open(SECRET_FILE, "w") as f:
                 print("We need to create a text file 'secret.txt' where "
                       "we will store your login and password from Instagram.")
                 print("Don't worry. It will be stored locally.")
@@ -39,6 +40,10 @@ def check_secret():
                 f.write(str(sys.stdin.readline()))
                 print("Enter your password: ")
                 f.write(str(sys.stdin.readline()))
+
+def delete_credentials():
+    if os.path.exists(SECRET_FILE):
+        os.remove(SECRET_FILE)
 
 
 if __name__ == "__main__":
