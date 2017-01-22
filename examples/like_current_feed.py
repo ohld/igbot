89a0ec6f7afff4493783ed7931e00bd97306963b
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-22 12:32:19 +0300

    adopt example for new api

--- examples/like_current_feed.py
+++ examples/like_current_feed.py
@@ -7,18 +7,19 @@ import sys, os
 sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import API
 
-def like_current_feed(api, amount):
-    """ Subscribes to people that are followed by username. """
-    print ("Going to like %d medias of your feed." % amount)
-    all_feed = api.get_feed(amount)
-    not_liked_feed = [item["id"] for item in all_feed if not item["likes"]["viewer_has_liked"]]
+def like_current_feed(api):
+    """ Likes last 8 medias from timeline feed """
+    print ("Going to like %d medias of your feed." % 8)
+    api.getTimelineFeed()
+
+    all_feed = api.LastJson["items"]
+    not_liked_feed = [item["pk"] for item in all_feed if not item["has_liked"]]
     print ("  Recieved %d of your last feed. You have already liked %d on them."%\
                             (len(all_feed), len(all_feed) - len(not_liked_feed)))
 
     total_liked = 0
     for media in not_liked_feed:
-        if total_liked % 10 == 0:
-            print ("  Total liked: %d" % total_liked)
+        print ("  Total liked: %d" % total_liked)
         if api.like(media):
             total_liked += 1
         else:
@@ -33,14 +34,14 @@ def like_current_feed(api, amount):
     return True
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print ("USAGE: python like_current_feed.py <amount of feed to like>")
-        exit()
-    if not sys.argv[1].isdigit():
-        print (" You should pass a number.")
-        exit()
+    # if len(sys.argv) != 2:
+    #     print ("USAGE: python like_current_feed.py <amount of feed to like>")
+    #     exit()
+    # if not sys.argv[1].isdigit():
+    #     print (" You should pass a number.")
+    #     exit()
 
     api = API()
     api.login()
-    like_current_feed(api, int(sys.argv[1]))
+    like_current_feed(api)
     api.logout()
