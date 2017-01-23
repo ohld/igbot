4ce7ee793d67b52e4d0aad0a87c4a640e8cd5441
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-23 23:23:27 +0300

    add checkpoints functions

--- instabot/bot/bot.py
+++ instabot/bot/bot.py
@@ -8,6 +8,10 @@ from .bot_like_feed import like_timeline
 from .bot_like_feed import like_user_id
 from .bot_unfollow_non_followers import unfollow_non_followers
 from .bot_like_hashtag import like_hashtag
+from .bot_checkpoint import save_checkpoint
+from .bot_checkpoint import load_checkpoint
+from .bot_checkpoint import checkpoint_followers_diff
+from .bot_checkpoint import checkpoint_following_diff
 
 class Bot(API):
     def __init__(self):
@@ -19,8 +23,10 @@ class Bot(API):
     def logout(self):
         super(self.__class__, self).logout()
         print ("Bot stopped.")
-        print ("""Total liked: %d, Total followed: %d""" % (
-                self.total_liked, self.total_followed
+        print ("""Total liked: %d,
+                  Total followed: %d,
+                  Total unfollowed: %d""" % (
+            self.total_liked, self.total_followed, self.total_unfollowed
         ))
 
     def like_medias(self, medias):
@@ -46,7 +52,7 @@ class Bot(API):
                 total_followed += 1
             else:
                 pass
-            time.sleep(10 + 10 * random.random())
+            time.sleep(15 + 30 * random.random())
         print ("    DONE: Total followed %d users. " % total_followed)
         self.total_followed += total_followed
         return True
@@ -66,13 +72,25 @@ class Bot(API):
         return True
 
     def like_timeline(self):
-        like_timeline(self)
+        return like_timeline(self)
 
     def like_user_id(self, user_id):
-        like_user_id(self, user_id)
+        return like_user_id(self, user_id)
 
     def unfollow_non_followers(self):
-        unfollow_non_followers(self)
+        return unfollow_non_followers(self)
 
     def like_hashtag(self, tag):
-        like_hashtag(self, tag)
+        return like_hashtag(self, tag)
+
+    def save_checkpoint(self):
+        return save_checkpoint(self)
+
+    def load_checkpoint(self, path):
+        return load_checkpoint(self, path)
+
+    def checkpoint_followers_diff(self, cp):
+        return checkpoint_followers_diff(self, cp)
+
+    def checkpoint_following_diff(self, cp):
+        return checkpoint_following_diff(self, cp)
