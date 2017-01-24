b47b3a7ec295dd316df843e9b16d57e92ea17b83
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-01-24 15:19:22 +0300

    add like-and-follow methods

---
+++ instabot/bot/bot_like_and_follow.py
@@ -0,0 +1,35 @@
+"""
+    Like-and-follow-users methods
+"""
+
+import time
+import random
+from tqdm import tqdm
+
+def like_and_follow(bot, user_id, nlikes=3):
+    """
+        Likes and follows <user_id>. Puts <nlikes> in his media.
+    """
+    bot.like_user_id(user_id, amount=nlikes)
+    bot.follow(user_id)
+    return True
+
+def like_and_follow_your_feed_likers(bot, nlikes=3):
+    """
+        Takes your last media. Takes the list of the likers.
+        Follows liker and like <nlikes> of his last posted medias.
+
+        args:
+            bot - instance of instabot.Bot() class
+            nlikes - number of likes to put on each user's media
+    """
+    print ("Going to like & follow users who like your last media")
+    bot.getSelfUserFeed()
+    last_media = bot.LastJson["items"][0]["pk"]
+    bot.getMediaLikers(last_media)
+    media_likers = [item['pk'] for item in bot.LastJson["users"]]
+    print ("  I have %d likers of your last media." % len(media_likers))
+    for user in tqdm(media_likers, desc="Media likers"):
+        bot.like_and_follow(user)
+        time.sleep(10 + 10 * random.random())
+    return True
