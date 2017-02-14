be92c0110aaa1b0e8c71f13796519c55c5da0488
ohld <ohl.d@yandex.ru>
2017-02-15 00:51:55 +0300

    move function at the top

--- instabot/bot/bot_comment.py
+++ instabot/bot/bot_comment.py
@@ -14,10 +14,43 @@ import random
 import os
 import io
 
+def comment(self, media_id, comment_text):
+    if not self.check_media(media_id):
+        return False
+    if super(self.__class__, self).comment(media_id, comment_text):
+        self.total_commented += 1
+        return True
+    return False
+
 def comment_hashtag(bot, hashtag, amount=None):
     bot.logger.info("Going to comment medias by %s hashtag" % hashtag)
     medias = bot.get_hashtag_medias(hashtag)
     return bot.comment_medias(medias[:amount])
+
+def comment_medias(self, medias):
+    """ medias - list of ["pk"] fields of response """
+    self.logger.info("    Going to comment on %d medias." % (len(medias)))
+    total_commented = 0
+    for media in tqdm(medias):
+
+        # grab a comment
+        co = self.get_comment('comments.txt')
+
+        if self.comment(media, co):
+            total_commented += 1
+        else:
+            pass
+        time.sleep(10 * random.random())
+    self.logger.info("    DONE: Total commented on %d medias. " % total_commented)
+    return True
+
+def comment_users(bot, user_ids):
+    # TODO: Put a comment to last media of every user from list
+    pass
+
+def comment_geotag(bot, geotag):
+    # TODO: comment every media from geotag
+    pass
 
 def get_random_comment_from_file(file_path):
     with io.open(file_path, "r", encoding="utf8") as f:
@@ -51,24 +84,3 @@ def is_commented(bot, media_id):
         return False
     usernames = [item["user"]["username"] for item in bot.LastJson['comments']]
     return bot.username in usernames
-
-def comment_medias(self, medias):
-    """ medias - list of ["pk"] fields of response """
-    self.logger.info("    Going to comment on %d medias." % (len(medias)))
-    total_commented = 0
-    for media in tqdm(medias):
-
-        # grab a comment
-        co = self.get_comment('comments.txt')
-
-        if self.comment(media, co):
-            total_commented += 1
-        else:
-            pass
-        time.sleep(10 * random.random())
-    self.logger.info("    DONE: Total commented on %d medias. " % total_commented)
-    return True
-
-def comment_users(bot, user_ids):
-    """ Put a comment to last media of every user from list"""
-    pass
