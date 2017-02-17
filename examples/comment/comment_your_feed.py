42b64cc5f5d7d713d461094d6218a7752e413b69
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-02-17 10:50:13 +0300

    add file path passage via argv

--- examples/comment/comment_your_feed.py
+++ examples/comment/comment_your_feed.py
@@ -2,10 +2,9 @@
     instabot example
 
     Dependencies:
-        You must have a "comments_emojie.txt" file which you can find here:
-        https://github.com/ohld/instabot/tree/master/examples/comment
-        This files contains comments to comment.
-
+        You must have a file with comments to post.
+        The file should have one comment per line.
+        
     Workflow:
         1) Get your timeline medias
         2) Comment them with random comments from file.
@@ -22,12 +21,17 @@ from tqdm import tqdm
 sys.path.append(os.path.join(sys.path[0],'../../'))
 from instabot import Bot
 
-comments_file_name = "comments_emojie.txt"
+if len(sys.argv) != 2:
+    print ("USAGE: Pass a path to the file with comments")
+    print ("Example: %s comments_emojie.txt" % sys.argv[0])
+    exit()
+
+comments_file_name = sys.argv[1]
 if not os.path.exists(comments_file_name):
     print ("Can't find '%s' file." % comments_file_name)
     exit()
 
-bot = Bot(comments_file="comments_emojie.txt")
+bot = Bot(comments_file=comments_file_name)
 bot.login()
 bot.comment_medias(bot.get_timeline_medias())
 bot.logout()
