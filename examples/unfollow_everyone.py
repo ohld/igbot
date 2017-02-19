ca758cc5fdd58836bcfba6533ca9e30defc82512
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-02-19 15:49:06 +0300

    add example unfollow everyone

---
+++ examples/unfollow_everyone.py
@@ -0,0 +1,17 @@
+"""
+    instabot example
+
+    Workflow:
+        1) unfollows every from your account.
+"""
+
+import sys
+import os
+
+sys.path.append(os.path.join(sys.path[0],'../'))
+
+from instabot import Bot
+
+bot = Bot()
+bot.login()
+bot.unfollow_everyone()
