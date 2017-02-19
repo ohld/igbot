ab322493d31d9be4d1c9d700c53bf0421e42932d
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-02-19 18:42:58 +0300

    add examples

---
+++ examples/like_user_followers.py
@@ -0,0 +1,24 @@
+"""
+    instabot example
+
+    Workflow:
+        Follow user's followers by user_id.
+"""
+
+import sys
+import os
+import time
+import random
+from tqdm import tqdm
+
+sys.path.append(os.path.join(sys.path[0],'../'))
+from instabot import Bot
+
+if len(sys.argv) != 2:
+    print ("USAGE: Pass username")
+    print ("Example: %s ohld" % sys.argv[0])
+    exit()
+
+bot = Bot()
+bot.login()
+bot.like_followers(sys.argv[1])
