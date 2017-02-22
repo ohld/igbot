72b09d7b92a1a47ce95f287418b7b361c204c97b
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-02-22 18:21:18 +0300

    add like_user example

---
+++ examples/like_user.py
@@ -0,0 +1,24 @@
+"""
+    instabot example
+
+    Workflow:
+        Like last medias by user.
+"""
+
+import sys
+import os
+import time
+import random
+from tqdm import tqdm
+
+sys.path.append(os.path.join(sys.path[0],'../'))
+from instabot import Bot
+
+if len(sys.argv) != 2:
+    print ("USAGE: Pass user to like")
+    print ("Example: %s ohld" % sys.argv[0])
+    exit()
+
+bot = Bot()
+bot.login()
+bot.like_user_id(sys.argv[1])
