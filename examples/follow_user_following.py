dd4fd529c5c35e59d05a463965065bb707f058c3
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-02-25 19:57:29 +0300

    update examples to held several usernames

--- examples/follow_user_following.py
+++ examples/follow_user_following.py
@@ -14,11 +14,12 @@ from tqdm import tqdm
 sys.path.append(os.path.join(sys.path[0],'../'))
 from instabot import Bot
 
-if len(sys.argv) != 2:
-    print ("USAGE: Pass username")
-    print ("Example: python %s ohld" % sys.argv[0])
+if len(sys.argv) <= 2:
+    print ("USAGE: Pass username / usernames.")
+    print ("Example: python %s ohld lenakolenka" % sys.argv[0])
     exit()
 
 bot = Bot()
 bot.login()
-bot.follow_following(sys.argv[1])
+for username in sys.argv[1:]:
+    bot.follow_following(username)
