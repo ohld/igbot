def326621663d64b09c2fa0bb22008ecb4041dd5
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-02-27 13:13:48 +0300

    add multiple hashtags support

--- examples/comment/comment_hashtags.py
+++ examples/comment/comment_hashtags.py
@@ -5,10 +5,6 @@
         You must have a file with comments to post.
         The file should have one comment per line.
 
-    Workflow:
-        1) Get your timeline medias
-        2) Comment them with random comments from file.
-
     Notes:
         You can change file and add there your comments.
 """
@@ -23,16 +19,17 @@ from instabot import Bot
 
 if len(sys.argv) < 3:
     print ("USAGE: Pass a path to the file with comments and an hastag to comment")
-    print ("Example: %s comments_emojie.txt dog" % sys.argv[0])
+    print ("Example: %s comments_emojie.txt dog cat" % sys.argv[0])
     exit()
 
 comments_file_name = sys.argv[1]
-hashtag = sys.argv[2]
+hashtags = sys.argv[2:]
 if not os.path.exists(comments_file_name):
     print ("Can't find '%s' file." % comments_file_name)
     exit()
 
 bot = Bot(comments_file=comments_file_name)
 bot.login()
-bot.comment_hashtag(hashtag)
+for hashtag in hashtags:
+    bot.comment_hashtag(hashtag)
 bot.logout()
