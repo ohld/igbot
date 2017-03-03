5c7bac30816b11a596ae78f614a24a8df5d6ec32
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-03-03 15:09:24 +0300

    add like_timeline_feed.py example

---
+++ examples/like_timeline_feed.py
@@ -0,0 +1,17 @@
+"""
+    instabot example
+
+    Workflow:
+        Like rescent medias from your timeline feed.
+"""
+
+import sys
+import os
+
+sys.path.append(os.path.join(sys.path[0],'../'))
+
+from instabot import Bot
+
+bot = Bot()
+bot.login()
+bot.like_timeline()
