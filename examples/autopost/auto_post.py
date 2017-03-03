bef2824389b7bcfd027e61aed2054b07c9d335e2
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-03-03 15:02:35 +0300

    fixing auto_post example bugs

--- examples/autopost/auto_post.py
+++ examples/autopost/auto_post.py
@@ -10,12 +10,15 @@ from instabot import Bot
 posted_pic_list = []
 try:
 	with open('pics.txt', 'r') as f:
-    	posted_pic_list = f.read().splitlines()
+		posted_pic_list = f.read().splitlines()
 except:
 	print("Please create pics.txt")
 	sys.exit()
 
-timeout = 24*60*60 # 24 hours	
+timeout = 24*60*60 # 24 hours
+
+bot = Bot()
+bot.login()
 
 while True:
 	pics = glob.glob("./pics/*.jpg")
@@ -24,29 +27,24 @@ while True:
 		for pic in pics:
 			if pic in posted_pic_list:
 				continue
-			
-			bot = Bot()
-			bot.login()
-			
+
 			caption = pic[:-4].split(" ")
 			caption = " ".join(caption[1:])
-			
+
 			print("upload: " + caption)
 			bot.uploadPhoto(pic, caption = caption)
 			if bot.LastResponse.status_code != 200:
 				print(bot.LastResponse)
 				#snd msg
 				break
-			
+
 			if not pic in posted_pic_list:
 				posted_pic_list.append(pic)
 				with open('pics.txt', 'a') as f:
 					f.write(pic+"\n")
-			
-			bot.logout()
 
 			time.sleep(timeout)
 
 	except Exception as e:
-		print(str(e)) 
-	time.sleep(60)	
+		print(str(e))
+	time.sleep(60)
