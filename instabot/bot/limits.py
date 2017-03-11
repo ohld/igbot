5f98cb70d730c1bf821a7bbe7f4ec059690c38bd
Pavel Pleskov <ppleskov@users.noreply.github.com>
2017-03-11 12:59:40 +0300

    small fix
    like/unlike was mixed up

--- instabot/bot/limits.py
+++ instabot/bot/limits.py
@@ -32,12 +32,12 @@ def check_if_bot_can_unfollow(bot):
     return bot.max_unfollows_per_day - bot.total_unfollowed > 0
 
 
-def check_if_bot_can_like(bot):
+def check_if_bot_can_unlike(bot):
     reset_if_day_passed(bot)
     return bot.max_unlikes_per_day - bot.total_unliked > 0
 
 
-def check_if_bot_can_unlike(bot):
+def check_if_bot_can_like(bot):
     reset_if_day_passed(bot)
     return bot.max_likes_per_day - bot.total_liked > 0
 
