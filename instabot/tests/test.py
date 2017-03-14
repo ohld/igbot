d9919e282816a91dba60737556a4ff5ccf0b0ad2
ohld <ohl.d@yandex.ru>
2017-03-14 21:11:06 +0300

    add assertion to the test.py

--- instabot/tests/test.py
+++ instabot/tests/test.py
@@ -1,7 +1,12 @@
+import sys
+import os
+
+sys.path.append(os.path.join(sys.path[0], '../../'))
 from instabot import Bot
 
 bot = Bot()
 bot.login()
-bot.like_timeline()
-bot.like_user("352300017")
-bot.follow_users(["352300017"])
+assert(bot.like_timeline() == [])
+assert(bot.like_user("352300017") == [])
+assert(bot.follow_users(["352300017"]) == [])
+assert(bot.like_hashtag("mipt") == [])
