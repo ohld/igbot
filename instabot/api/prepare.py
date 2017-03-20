34c2d6ef79200c06b8a4385289dec80a650520ca
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-03-20 17:46:06 +0300

    add the ability to add credentials without secret.txt deletion

--- instabot/api/prepare.py
+++ instabot/api/prepare.py
@@ -6,25 +6,34 @@ import getpass
 SECRET_FILE = "secret.txt"
 
 
+def add_credentials():
+    with open(SECRET_FILE, "a") as f:
+        print("Enter your login: ")
+        f.write(str(sys.stdin.readline().strip()) + ":")
+        print("Enter your password: ")
+        f.write(getpass.getpass() + "\n")
+
+
 def get_credentials():
     "Returns login and password stored in SECRET_FILE"
     while not check_secret():
         pass
-    with open(SECRET_FILE, "r") as f:
-        lines = [line.strip().split(":") for line in f.readlines()]  # '\n'
-        if len(lines) == 1:
-            return lines[0]
-        else:
-            print("Which account do you want to use? (Type number)")
-            for ind, (login, password) in enumerate(lines):
-                print(ind, login)
-            while True:
-                try:
-                    ind = int(sys.stdin.readline())
-                    if ind in list(range(len(lines))):
-                        return lines[ind]
-                except:
-                    print("Wrong input. I need the number of account to use.")
+    while True:
+        with open(SECRET_FILE, "r") as f:
+            lines = [line.strip().split(":") for line in f.readlines()]
+        print("Which account do you want to use? (Type number)")
+        for ind, (login, password) in enumerate(lines):
+            print("%d: %s" % (ind + 1, login))
+        print("%d: %s" % (0, "add another account."))
+        try:
+            ind = int(sys.stdin.readline())
+            if ind == 0:
+                add_credentials()
+                continue
+            if ind - 1 in list(range(len(lines))):
+                return lines[ind - 1]
+        except:
+            print("Wrong input. I need the number of account to use.")
 
 
 def check_secret():
@@ -46,18 +55,14 @@ def check_secret():
                           "and try again.")
                     os.remove(SECRET_FILE)
         else:
-            with open(SECRET_FILE, "w") as f:
-                print("We need to create a text file 'secret.txt' where "
-                      "we will store your login and password from Instagram.")
-                print("Don't worry. It will be stored locally.")
-                while True:
-                    print("Enter your login: ")
-                    f.write(str(sys.stdin.readline().strip()) + ":")
-                    print("Enter your password: ")
-                    f.write(getpass.getpass() + "\n")
-                    print("Do you want to add another account? (y/n)")
-                    if "y" not in sys.stdin.readline():
-                        break
+            print("We need to create a text file '%s' where "
+                  "we will store your login and password from Instagram." % SECRET_FILE)
+            print("Don't worry. It will be stored locally.")
+            while True:
+                add_credentials()
+                print("Do you want to add another account? (y/n)")
+                if "y" not in sys.stdin.readline():
+                    break
 
 
 def delete_credentials():
