a432b27bea2c4cf89fbf19e290f06105b147e5aa
filatovzru <ivan@filatovz.ru>
2017-03-28 15:15:30 +0300

    Create config_bot.py чтобы можно было использовать одни настройки для всех скриптов

--- examples/multi_script_CLI.py
+++ examples/multi_script_CLI.py
@@ -4,7 +4,9 @@ import os
 from tqdm import tqdm
 
 sys.path.append(os.path.join(sys.path[0], '../'))
-from instabot import Bot
+from examples import config_bot
+bot = config_bot.bot
+
 
 if hasattr(__builtins__, 'raw_input'):
     input = raw_input
@@ -67,30 +69,6 @@ def menu():
         else:
             print("\n Not valid choice. Try again")
 
-
-bot = Bot(
-    max_likes_per_day=1000,
-    max_unlikes_per_day=1000,
-    max_follows_per_day=350,
-    max_unfollows_per_day=350,
-    max_comments_per_day=100,
-    max_likes_to_like=100,
-    max_followers_to_follow=2000,
-    min_followers_to_follow=250,
-    max_following_to_follow=10000,
-    min_following_to_follow=10,
-    max_followers_to_following_ratio=10,
-    max_following_to_followers_ratio=2,
-    min_media_count_to_follow=7,
-    like_delay=10,
-    unlike_delay=10,
-    follow_delay=30,
-    unfollow_delay=30,
-    comment_delay=60,
-    stop_words=['order', 'shop', 'store', 'free', 'doodleartindonesia',
-                'doodle art indonesia', 'fullofdoodleart', 'commission',
-                'vector', 'karikatur', 'jasa', 'open']
-)
 bot.logger.info("Multi script run")
 print('Hi, welcome to instabot. I will guide you.')
 bot.login()
