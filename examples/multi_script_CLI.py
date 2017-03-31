4bde2469a0c8f2145a176357f3e01f4147d332f9
filatovzru <ivan@filatovz.ru>
2017-03-31 11:21:17 +0300

    ImportError: No module named past.builtins

--- examples/multi_script_CLI.py
+++ examples/multi_script_CLI.py
@@ -3,10 +3,9 @@ import time
 import sys
 import os
 
-from past.builtins import raw_input
-
 sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
+
 
 if hasattr(__builtins__, 'raw_input'):
     input = raw_input
@@ -79,7 +78,8 @@ def menu():
 
 
 def getFollowersToFile(self, user):
-    import random, re
+    import random
+    import re
     from tqdm import tqdm
     followers = self.getTotalFollowers(self.convert_to_user_id(user))
     out_file_name = 'followers_of_%s.tsv' % user
@@ -90,11 +90,15 @@ def getFollowersToFile(self, user):
     for u_name in tqdm(followers, desc='Getting [ %s ] followers' % user):
         try:
             user_info = self.get_user_info(u_name['pk'])
-            info = str(str(user_info['full_name']).replace('\n', '').replace(' ', '') + '\t' + user_info['username'] + '\t' + str(user_info['pk']) + '\t' + str(user_info['biography']).replace('\n','') + '\t' + str(user_info['follower_count']) + '\t' + str(user_info['following_count']) + '\t' + str(user_info['is_business']) + '\t' + user_info['profile_pic_url'] + '\n')
-            info = re.sub(r'[^\w+|\s|\w|\.|\/]',' ',info)
+            info = str(str(user_info['full_name']).replace('\n', '').replace(' ', '') + '\t' +
+                       user_info['username'] + '\t' +
+                       str(user_info['pk']) + '\t' +
+                       str(user_info['biography']).replace('\n', '') + '\t' +
+                       str(user_info['follower_count']) + '\t' + str(user_info['following_count']) + '\t' +
+                       str(user_info['is_business']) + '\t' + user_info['profile_pic_url'] + '\n')
+            info = re.sub(r"[^\w+|\s|\w|\.|\/]", ' ', info)
             out_file.write(info)
             i = i + 1
-            # self.logger.info('[%s|%s] %s is added ---> %s' % (str(i), len(followers), u_name['username'], out_file_name))
             time.sleep(random.randrange(1, 10))  # Picked up empirically
         except Exception as e:
             self.logger.warning('User %s not write because: %s' % (user_info['username'], e))
