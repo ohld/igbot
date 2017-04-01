36a67caa54e96dd9c90aa7a09987a886b63014a6
Okhlopkov Daniil Olegovich <ohl.d@yandex.ru>
2017-04-01 20:38:57 +0300

    add argparse to the examples

--- examples/unfollow_non_followers.py
+++ examples/unfollow_non_followers.py
@@ -7,11 +7,19 @@
 
 import sys
 import os
+import argparse
 
 sys.path.append(os.path.join(sys.path[0], '../'))
 
 from instabot import Bot
 
+parser = argparse.ArgumentParser(add_help=True)
+parser.add_argument('-u', type=str, help="username")
+parser.add_argument('-p', type=str, help="password")
+parser.add_argument('-proxy', type=str, help="proxy")
+args = parser.parse_args()
+
 bot = Bot()
-bot.login()
+bot.login(username=args.u, password=args.p,
+          proxy=args.proxy)
 bot.unfollow_non_followers()
