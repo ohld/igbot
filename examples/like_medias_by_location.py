67da05d8e53605ba63593236d3b6a6f97778404e
sudoguy <eskemerov@gmail.com>
2017-04-13 17:22:51 +0700

    Setting amount of likes.

--- examples/like_medias_by_location.py
+++ examples/like_medias_by_location.py
@@ -3,7 +3,7 @@
     instabot example
 
     Workflow:
-        Like last medias by location.
+        Like medias by location.
 """
 
 import argparse
@@ -16,16 +16,25 @@ sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
 
 
-def like_location_feed(new_bot, new_location):
-    if new_bot.getLocationFeed(new_location['location']['pk']):
-        for media in tqdm(new_bot.LastJson["items"]):
-            bot.like(media['id'])
-        return True
-    return False
+def like_location_feed(new_bot, new_location, amount=0):
+    counter = 0
+    max_id = ''
+    while counter < amount:
+        if new_bot.getLocationFeed(new_location['location']['pk'], maxid=max_id):
+            location_feed = new_bot.LastJson
+            for media in tqdm(location_feed["items"]):
+                if bot.like(media['id']):
+                    counter += 1
+            if location_feed.get('next_max_id'):
+                max_id = location_feed['next_max_id']
+            else:
+                return False
+    return True
 
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('-u', type=str, help="username")
 parser.add_argument('-p', type=str, help="password")
+parser.add_argument('-amount', type=str, help="amount")
 parser.add_argument('-proxy', type=str, help="proxy")
 parser.add_argument('locations', type=str, nargs='*', help='locations')
 args = parser.parse_args()
@@ -41,14 +50,23 @@ if args.locations:
         bot.searchLocation(location)
         finded_location = bot.LastJson['items'][0]
         if finded_location:
-            print(u"Finded {}".format(finded_location['title']))
-            like_location_feed(bot, finded_location)
+            print(u"Found {}".format(finded_location['title']))
+
+            if not args.amount:
+                nlikes = input("How much likes?")
+            else:
+                nlikes = args.amount
+            like_location_feed(bot, finded_location, amount=int(nlikes))
 else:
     location_name = raw_input(u"Write location name:\n").strip()
     bot.searchLocation(location_name)
     if not bot.LastJson['items']:
         print(u'Location was not found')
         exit(1)
+    if not args.amount:
+        nlikes = input(u"How much likes?")
+    else:
+        nlikes = args.amount
     ans = True
     while ans:
         for n, location in enumerate(bot.LastJson["items"], start=1):
@@ -60,6 +78,6 @@ else:
         try:
             ans = int(ans) - 1
             if ans in range(len(bot.LastJson["items"])):
-                like_location_feed(bot, bot.LastJson["items"][ans])
+                like_location_feed(bot, bot.LastJson["items"][ans], amount=int(nlikes))
         except ValueError:
             print(u"\n Not valid choice. Try again")
