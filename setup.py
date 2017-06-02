5803f1f277dcecdd2c43f682c8fce215a31dfcd6
ohld <ohl.d@yandex.ru>
2017-06-02 16:53:27 +0300

    update version and remove unnecessary packages

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
     name='instabot',
     packages=['instabot', 'instabot.bot', 'instabot.api'],
-    version='0.3.3.16',
+    version='0.3.3.18',
     description='Cool Instagram bot scripts and API python wrapper.',
     author='Daniil Okhlopkov',
     author_email='ohld@icloud.com',
     url='https://github.com/instagrambot/instabot',
-    download_url='https://github.com/ohld/instabot/tarball/0.3.3.16',
+    download_url='https://github.com/ohld/instabot/tarball/0.3.3.18',
     keywords=['instagram', 'bot', 'api', 'wrapper'],
     classifiers=[],
-    install_requires=['tqdm', 'requests-toolbelt', 'requests', 'requests', 'pep8', 'pyflakes', 'schedule'],
+    install_requires=['tqdm', 'requests-toolbelt', 'requests', 'schedule'],
 )
