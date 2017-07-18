987ec7121701c322ac0a3a04f2fef249885b60ec
mghayour <me@mghayour.ir>
2017-07-18 16:20:44 +0430

    put video api in seperate file, remove moviepy dependency and some bugfix

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -7,17 +7,16 @@ import uuid
 import sys
 import logging
 import time
-import copy
-import math
 from random import randint
 from tqdm import tqdm
-from requests_toolbelt import MultipartEncoder
-from moviepy.editor import VideoFileClip
 
 from . import config
 from .api_photo import configurePhoto
 from .api_photo import uploadPhoto
 from .api_photo import downloadPhoto
+
+from .api_video import configureVideo
+from .api_video import uploadVideo
 
 from .api_search import fbUserSearch
 from .api_search import searchUsers
@@ -34,6 +33,7 @@ from .api_profile import setNameAndPhone
 
 from .prepare import get_credentials
 from .prepare import delete_credentials
+
 
 # The urllib library was split into other modules from Python 2 to Python 3
 if sys.version_info.major == 3:
@@ -196,6 +196,12 @@ class API(object):
 
     def configurePhoto(self, upload_id, photo, caption=''):
         return configurePhoto(self, upload_id, photo, caption)
+
+    def uploadVideo(self, photo, caption=None, upload_id=None):
+        return uploadVideo(self, photo, caption, upload_id)
+
+    def configureVideo(self, upload_id, video, thumbnail, caption = ''):
+        return configureVideo(self, upload_id, video, thumbnail, caption)
 
     def editMedia(self, mediaId, captionText=''):
         data = json.dumps({
@@ -576,9 +582,11 @@ class API(object):
         while 1:
             self.getUserFeed(usernameId, next_max_id, minTimestamp)
             temp = self.LastJson
+            if "items" not in temp: # maybe user is private, (we have not access to posts)
+                return []
             for item in temp["items"]:
                 user_feed.append(item)
-            if temp["more_available"] is False:
+            if "more_available" not in temp or temp["more_available"] is False:
                 return user_feed
             next_max_id = temp["next_max_id"]
 
@@ -601,92 +609,3 @@ class API(object):
             for item in temp["items"]:
                 liked_items.append(item)
         return liked_items
-
-    def configureVideo(self, upload_id, video, thumbnail, caption=''):
-        clip = VideoFileClip(video)
-        self.uploadPhoto(photo=thumbnail, caption=caption, upload_id=upload_id)
-        data = json.dumps({
-            'upload_id': upload_id,
-            'source_type': 3,
-            'poster_frame_index': 0,
-            'length': 0.00,
-            'audio_muted': False,
-            'filter_type': 0,
-            'video_result': 'deprecated',
-            'clips': {
-                'length': clip.duration,
-                'source_type': '3',
-                'camera_position': 'back',
-            },
-            'extra': {
-                'source_width': clip.size[0],
-                'source_height': clip.size[1],
-            },
-            'device': config.DEVICE_SETTINTS,
-            '_csrftoken': self.token,
-            '_uuid': self.uuid,
-            '_uid': self.user_id,
-            'caption': caption,
-        })
-        return self.SendRequest('media/configure/?video=1', self.generateSignature(data))
-
-    def uploadVideo(self, video, thumbnail, caption=None, upload_id=None):
-        if upload_id is None:
-            upload_id = str(int(time.time() * 1000))
-        data = {
-            'upload_id': upload_id,
-            '_csrftoken': self.token,
-            'media_type': '2',
-            '_uuid': self.uuid,
-        }
-        m = MultipartEncoder(data, boundary=self.uuid)
-        self.session.headers.update({'X-IG-Capabilities': '3Q4=',
-                                     'X-IG-Connection-Type': 'WIFI',
-                                     'Host': 'i.instagram.com',
-                                     'Cookie2': '$Version=1',
-                                     'Accept-Language': 'en-US',
-                                     'Accept-Encoding': 'gzip, deflate',
-                                     'Content-type': m.content_type,
-                                     'Connection': 'keep-alive',
-                                     'User-Agent': config.USER_AGENT})
-        response = self.session.post(config.API_URL + "upload/video/", data=m.to_string())
-        if response.status_code == 200:
-            body = json.loads(response.text)
-            upload_url = body['video_upload_urls'][3]['url']
-            upload_job = body['video_upload_urls'][3]['job']
-
-            videoData = open(video, 'rb').read()
-            request_size = int(math.floor(len(videoData) / 4))
-            lastRequestExtra = (len(videoData) - (request_size * 3))
-
-            headers = copy.deepcopy(self.session.headers)
-            self.session.headers.update({'X-IG-Capabilities': '3Q4=',
-                                         'X-IG-Connection-Type': 'WIFI',
-                                         'Cookie2': '$Version=1',
-                                         'Accept-Language': 'en-US',
-                                         'Accept-Encoding': 'gzip, deflate',
-                                         'Content-type': 'application/octet-stream',
-                                         'Session-ID': upload_id,
-                                         'Connection': 'keep-alive',
-                                         'Content-Disposition': 'attachment; filename="video.mov"',
-                                         'job': upload_job,
-                                         'Host': 'upload.instagram.com',
-                                         'User-Agent': config.USER_AGENT})
-            for i in range(0, 4):
-                start = i * request_size
-                if i == 3:
-                    end = i * request_size + lastRequestExtra
-                else:
-                    end = (i + 1) * request_size
-                length = lastRequestExtra if i == 3 else request_size
-                content_range = "bytes {start}-{end}/{lenVideo}".format(start=start, end=(end - 1),
-                                                                        lenVideo=len(videoData)).encode('utf-8')
-
-                self.session.headers.update({'Content-Length': str(end - start), 'Content-Range': content_range, })
-                response = self.session.post(upload_url, data=videoData[start:start + length])
-            self.session.headers = headers
-
-            if response.status_code == 200:
-                if self.configureVideo(upload_id, video, thumbnail, caption):
-                    return self.expose()
-        return False
