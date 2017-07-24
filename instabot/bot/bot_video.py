e31e26ca4dd7a19833daa1a18d4d51b753c862a5
Evgeny Kemerov <sintezz777@gmail.com>
2017-07-24 12:11:06 +0700

    Update bot_video.py

--- instabot/bot/bot_video.py
+++ instabot/bot/bot_video.py
@@ -5,7 +5,7 @@ def upload_video(self, video, thumbnail, caption=''):
     delay.small_delay(self)
     self.logger.info("Started uploading '{video}'".format(video=video))
     if not super(self.__class__, self).uploadVideo(video, thumbnail, caption):
-        self.logger.info("Video '%s' is not %s ." % video, 'uploaded')
+        self.logger.info("Video '%s' is not %s ." % (video, 'uploaded'))
         return False
     self.logger.info("Video '{video}' uploaded".format(video=video))
     return True
