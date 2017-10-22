2fd75abe3bcb48c2b5d61bef64ddfb751458335e
Catalin <bardascat@gmail.com>
2017-10-22 22:39:35 +0300

    bot log adjustment

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -158,23 +158,23 @@ class API(object):
             self.LastJson = json.loads(response.text)
             return True
         else:
+            self.logger.info("Request error url: %s: ", config.API_URL + endpoint)
 
             if response.status_code != 404:
                 self.logger.warning("HTTP ERROR: STATUS %s , BODY: %s " % (str(response.status_code), response.text))
             else:
                 self.logger.warning("HTTP ERROR: STATUS %s" % (str(response.status_code)))
 
-            self.logger.info("REQUEST ERROR: %s: ",config.API_URL + endpoint)
 
             if response.status_code == 400:
                 response = json.loads(response.text)
                 if 'spam' in response:
-                    self.logger.info('SPAM IS IN RESPONSE %s',response)
+                    sleep_minutes = 10
+                    self.logger.warning("BOT IS BLOCKED, going to sleep %s minutes" % sleep_minutes)
+                    time.sleep(sleep_minutes * 60)
                 else:
-                    self.logger.info('SPAM IS NOT IN RESPONSE %s', response)
-                sleep_minutes = 10
-                self.logger.warning("BOT IS BLOCKED, going to sleep %s minutes" % sleep_minutes)
-                time.sleep(sleep_minutes * 60)
+                    sleep_minutes=1
+                    self.logger.warning("Request return 400 error. Going to sleep %s minutes" % sleep_minutes)
 
             elif response.status_code == 429:
                 sleep_minutes = 5
