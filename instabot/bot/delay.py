ab321c67ac7d2b4a8cdd9a9c7c78bd16408b7d08
romankk <romankk@gmail.com>
2018-01-14 11:35:53 +0300

    Revert "Handle unhandled error on follow_users method"
    This reverts commit d535a091cd135eb88827977c8bd8b82dbabd878b.

--- instabot/bot/delay.py
+++ instabot/bot/delay.py
@@ -58,10 +58,6 @@ def error_delay(bot):
     time.sleep(10)
 
 
-def delay_in_seconds(bot, delay_time=60):
-    time.sleep(delay_time)
-
-
 def small_delay(bot):
     time.sleep(add_dispersion(3))
 
