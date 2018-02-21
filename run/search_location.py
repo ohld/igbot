5776dc9c0f6beeea70088699e8c13e4453b571b8
bardascat <bardascat@gmail.com>
2018-02-22 00:30:34 +0200

    search location

--- run/search_location.py
+++ run/search_location.py
@@ -4,6 +4,8 @@ import codecs
 import os
 import sys
 import json
+from instabot.api import api_db
+from instabot import Bot
 
 stdout = sys.stdout
 sys.stdout = codecs.getwriter('utf8')(sys.stdout)
@@ -11,35 +13,33 @@ sys.path.append(os.path.join(sys.path[0], '../'))
 
 parser = argparse.ArgumentParser(add_help=True)
 parser.add_argument('-location', type=str, help="location")
-parser.add_argument('-u', type=str, help="username")
-parser.add_argument('-p', type=str, help="password")
 parser.add_argument('-id_campaign', type=str, help="campaign")
 args = parser.parse_args()
 
 if not args.u or not args.p or not args.location:
     exit(0)
 
-from instabot import Bot
+campaign = api_db.fetchOne("select username,password,timestamp,id_campaign from campaign where id_campaign=%s",
+                           args.angie_campaign)
 
-bot = Bot(id_campaign=args.id_campaign,multiple_ip=True, hide_output=True)
+bot = Bot(id_campaign=args.id_campaign, multiple_ip=True, hide_output=True)
 bot.logger.info("search_location:Going to search the location %s" % (args.location))
 
-status  = bot.login(username=args.u,password=args.p)
-if status!=True:
-  print(bot.LastResponse.text)
-  exit()
+status = bot.login(username=campaign['username'], password=campaign['password'])
+if status != True:
+    print(bot.LastResponse.text)
+    exit()
 result = bot.searchLocation(query=args.location)
-parsedResult=[]
+parsedResult = []
 
 if not result:
     exit(0)
 else:
     for item in result:
-        r={}
-        r['id']=item['location']['pk']
-        r['name']=item['title']
+        r = {}
+        r['id'] = item['location']['pk']
+        r['name'] = item['title']
         parsedResult.append(r)
 
-    result=json.dumps(parsedResult)
+    result = json.dumps(parsedResult)
     print(result)
-
