5037c91a92511424127cfb7ba31010ea1527be48
gyunter <gyunter@gmail.com>
2018-03-07 14:15:13 +0300

    Remove spaces from blank lines

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -53,19 +53,19 @@ class API(object):
 
         # handle logging
         self.logger = logging.getLogger('[instabot]')
-        
+
         fh = logging.FileHandler(filename='instabot.log')
         fh.setLevel(logging.INFO)
         formatter = logging.Formatter(
             '%(asctime)s %(message)s')
         fh.setFormatter(formatter)
-  
+
         ch = logging.StreamHandler()
         ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter(
             '%(asctime)s - %(levelname)s - %(message)s')
         ch.setFormatter(formatter)
-        
+
         self.logger.addHandler(fh)
         self.logger.addHandler(ch)
         self.logger.setLevel(logging.DEBUG)
