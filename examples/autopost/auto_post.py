e83019f67a3c93efac27566666bcff5eb0d2a0da
sudoguy <eskemerov@gmail.com>
2018-03-20 11:55:10 +0300

    Add utf-8 in autopost example

--- examples/autopost/auto_post.py
+++ examples/autopost/auto_post.py
@@ -1,14 +1,16 @@
-import time
-import sys
-import os
 import glob
+import os
+import sys
+import time
+
+from io import open
 
 sys.path.append(os.path.join(sys.path[0], '../../'))
 from instabot import Bot
 
 posted_pic_list = []
 try:
-    with open('pics.txt', 'r') as f:
+    with open('pics.txt', 'r', encoding='utf8') as f:
         posted_pic_list = f.read().splitlines()
 except Exception:
     posted_pic_list = []
@@ -38,7 +40,7 @@ while True:
 
             if pic not in posted_pic_list:
                 posted_pic_list.append(pic)
-                with open('pics.txt', 'a') as f:
+                with open('pics.txt', 'a', encoding='utf8') as f:
                     f.write(pic + "\n")
 
             time.sleep(timeout)
