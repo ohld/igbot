26d721ef901ff600f352f0e04af887369c6a151b
Bas Nijholt <basnijholt@gmail.com>
2018-04-20 19:15:42 +0200

    Remove brackets that are not needed and do not call list when not needed

--- examples/ultimate_schedule/ultimate.py
+++ examples/ultimate_schedule/ultimate.py
@@ -115,7 +115,7 @@ def job10():  # put non followers on blacklist
         followings = bot.get_user_following(bot.user_id)  # getting following
         followers = bot.get_user_followers(bot.user_id)  # getting followers
         friends_file = bot.read_list_from_file("friends.txt")  # same whitelist (just user ids)
-        nonfollowerslist = list((set(followings) - set(followers)) - set(friends_file))
+        nonfollowerslist = set(followings) - set(followers) - set(friends_file)
         with open(config.BLACKLIST_FILE, 'a') as file:  # writing to the blacklist
             for user_id in nonfollowerslist:
                 file.write(str(user_id) + "\n")
--- instabot/bot/bot_unfollow.py
+++ instabot/bot/bot_unfollow.py
@@ -66,7 +66,7 @@ def update_unfollow_file(self):
     followings = self.get_user_following(self.user_id)
     followers = self.get_user_followers(self.user_id)
     friends_file = self.read_list_from_file("friends.txt")  # same whitelist (just user ids)
-    nonfollowerslist = list((set(followings) - set(followers)) - set(friends_file))
+    nonfollowerslist = set(followings) - set(followers) - set(friends_file)
     followed_list = self.read_list_from_file("followed.txt")
     unfollow_list = [x for x in followed_list if x in nonfollowerslist]
     unfollow_list += [x for x in nonfollowerslist if x not in followed_list]
