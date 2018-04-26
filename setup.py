48507b16f5e808dffaf0c4a0c9299981913212d7
sudoguy <eskemerov@gmail.com>
2018-04-26 16:21:36 +0300

    Update version link

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author='Daniil Okhlopkov, Evgeny Kemerov',
     author_email='ohld@icloud.com, eskemerov@gmail.com',
     url='https://github.com/instagrambot/instabot',
-    download_url='https://github.com/instagrambot/instabot/tarball/0.4',
+    download_url='https://github.com/instagrambot/instabot/tarball/0.4.1',
     keywords=['instagram', 'bot', 'api', 'wrapper'],
     classifiers=[],
     install_requires=['tqdm', 'requests-toolbelt', 'requests', 'schedule', 'future', 'six', 'huepy'],
