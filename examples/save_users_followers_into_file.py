ee4f034c386b2cf387feb76be891d2c32676057b
Omar Olivares <ofjolivares@gmail.com>
2018-05-18 13:36:58 -0400

    Add filename to args

--- examples/save_users_followers_into_file.py
+++ examples/save_users_followers_into_file.py
@@ -7,14 +7,15 @@
 import os
 import sys
 import argparse
-
-sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
+sys.path.append(os.path.join(sys.path[0], '../'))
+
 
 parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-u', type=str, help="username")
 parser.add_argument('-p', type=str, help="password")
 parser.add_argument('-proxy', type=str, help="proxy")
+parser.add_argument('filename', type=str, help="filename")
 parser.add_argument('users', type=str, nargs='+', help='users')
 args = parser.parse_args()
 
@@ -22,7 +23,7 @@ bot = Bot()
 bot.login(username=args.u, password=args.p,
           proxy=args.proxy)
 
-fh = open("users_followers.txt", "a+")
+fh = open(args.filename, "a+")
 for username in args.users:
     followers = bot.get_user_followers(username)
     for user in followers:
