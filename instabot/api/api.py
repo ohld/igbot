8bb0e98cbce98726007553baf50017e788786cc5
Bas Nijholt <basnijholt@gmail.com>
2018-06-04 23:50:09 -0700

    simplify `get_total_user_feed` in api.py

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -600,18 +600,7 @@ class API(object):
             user_id, amount, 'followings')
 
     def get_total_user_feed(self, user_id, min_timestamp=None):
-        user_feed = []
-        next_max_id = ''
-        while True:
-            self.get_user_feed(user_id, next_max_id, min_timestamp)
-            last_json = self.last_json
-            if "items" not in last_json:
-                # User is private, we have no access to the posts
-                return []
-            user_feed += last_json["items"]
-            if not last_json.get("more_available"):
-                return user_feed
-            next_max_id = last_json.get("next_max_id", "")
+        return self.get_last_user_feed(user_id, amount=float('inf'), min_timestamp=min_timestamp)
 
     def get_last_user_feed(self, user_id, amount, min_timestamp=None):
         user_feed = []
