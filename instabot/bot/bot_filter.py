af52697067a734afda167d124b814bf502584915
Olcaey <olcaey.sari@gmail.com>
2018-09-29 10:05:13 +0300

    Update bot_filter.py

--- instabot/bot/bot_filter.py
+++ instabot/bot/bot_filter.py
@@ -96,10 +96,7 @@ def search_stop_words_in_user(self, user_info):
 
 def search_blacklist_hashtags_in_media(self, media_id):
     media_info = self.get_media_info(media_id)
-    if media_info[0]['caption'] is None:
-        text = ''
-    else:
-        text = media_info[0]['caption']['text']
+    text = media_info[0]['caption']['text'] if media_info[0]['caption'] else ''
 
     media_comments = self.get_media_comments(media_id)
     comments_number = min(6, len(media_comments))
