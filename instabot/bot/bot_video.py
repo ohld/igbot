50338ebcc3544c3818aefcdb9f4db65ec1eb975a
Max Pierini <github@maxpierini.it>
2019-01-12 11:00:32 +0100

    Updated upload_video

--- instabot/bot/bot_video.py
+++ instabot/bot/bot_video.py
@@ -1,8 +1,8 @@
 
-def upload_video(self, video, thumbnail, caption=''):
+def upload_video(self, video, caption='', thumbnail=None):
     self.small_delay()
     self.logger.info("Started uploading '{video}'".format(video=video))
-    if not self.api.upload_video(video, thumbnail, caption):
+    if not self.api.upload_video(video, caption=caption, thumbnail=thumbnail):
         self.logger.info("Video '%s' is not %s ." % (video, 'uploaded'))
         return False
     self.logger.info("Video '{video}' uploaded".format(video=video))
