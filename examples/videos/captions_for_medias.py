35c6f72180a2518769b8d25a7487e2e8d8df985e
Max Pierini <github@maxpierini.it>
2019-01-12 11:12:03 +0100

    captions for videos to upload

---
+++ examples/videos/captions_for_medias.py
@@ -0,0 +1,4 @@
+CAPTIONS = {
+    "media_name_01.ext": "The caption with #hashtags and @mentions for this media",
+    "media_name_02.ext": "The caption with #hashtags and @mentions for this media",
+}
