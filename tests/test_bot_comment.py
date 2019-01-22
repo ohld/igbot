464a4bbfc32f548c3f38f3e1b211d59d2dfddf4d
maxdevblock <maxdevblock@maxpierini.it>
2019-01-22 09:33:55 +0100

    Added test comment status ok

--- tests/test_bot_comment.py
+++ tests/test_bot_comment.py
@@ -20,7 +20,7 @@ class TestBotGet(TestBot):
         (True, True),
         (True, False),
         (False, True),
-        (False, True)])
+        (False, False)])
     @patch('time.sleep', return_value=None)
     def test_comment_feedback(self, patched_time_sleep, blocked_actions_protection, blocked_actions):
         self.bot.blocked_actions_protection = blocked_actions_protection
@@ -64,3 +64,43 @@ class TestBotGet(TestBot):
         )
 
         assert not self.bot.comment(media_id, comment_txt)
+
+    @responses.activate
+    @pytest.mark.parametrize('blocked_actions_protection,blocked_actions', [
+        (True, False),
+        (False, False)])
+    @patch('time.sleep', return_value=None)
+    def test_comment(self, patched_time_sleep, blocked_actions_protection, blocked_actions):
+        self.bot.blocked_actions_protection = blocked_actions_protection
+        self.bot.blocked_actions['comments'] = blocked_actions
+        media_id = 1234567890
+        comment_txt = "Yeah great!"
+
+        TEST_COMMENT_ITEM['user']['pk'] = self.bot.user_id + 1
+
+        results = 3
+        response_data = {
+            "caption": TEST_CAPTION_ITEM,
+            "caption_is_edited": False,
+            "comment_count": results,
+            "comment_likes_enabled": True,
+            "comments": [TEST_COMMENT_ITEM for _ in range(results)],
+            "has_more_comments": False,
+            "has_more_headload_comments": False,
+            "media_header_display": "none",
+            "preview_comments": [],
+            "status": "ok"
+        }
+        responses.add(
+            responses.GET, '{api_url}media/{media_id}/comments/?'.format(
+                api_url=API_URL, media_id=media_id), json=response_data, status=200)
+
+        response_data = {
+            "status": "ok"}
+        responses.add(
+            responses.POST, '{api_url}media/{media_id}/comment/'.format(
+                api_url=API_URL, media_id=media_id
+            ), json=response_data, status=200
+        )
+
+        assert self.bot.comment(media_id, comment_txt)
