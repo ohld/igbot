dc4493bde8f1254f4c2a96f6245c0011f8c8364d
khairiazim <khairiazim@users.noreply.github.com>
2019-02-15 00:27:13 +0800

    do not upload photo

--- examples/ultimate_schedule/ultimate.py
+++ examples/ultimate_schedule/ultimate.py
@@ -127,7 +127,7 @@ schedule.every(16).hours.do(run_threaded, comment_medias)
 schedule.every(1).days.at("08:00").do(run_threaded, unfollow_non_followers)
 schedule.every(12).hours.do(run_threaded, follow_users_from_hashtag_file)
 schedule.every(6).hours.do(run_threaded, comment_hashtag)
-schedule.every(1).days.at("21:28").do(run_threaded, upload_pictures)
+#schedule.every(1).days.at("21:28").do(run_threaded, upload_pictures)
 schedule.every(4).days.at("07:50").do(run_threaded, put_non_followers_on_blacklist)
 
 while True:
