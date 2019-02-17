4dfdabebd13c75ab365502331e2d58da76b12732
lex <you@example.com>
2019-02-17 18:11:43 +0400

    adding personall Database files for each Instagram accounts

---
+++ instabot/bot/bot_files.py
@@ -0,0 +1,22 @@
+from .. import utils
+
+def whitelist(self):
+    return utils.file(self.username + '_whitelist.txt')
+
+def blacklist(self):
+    return utils.file(self.username + '_blacklist.txt')
+
+def comments(self):
+    return utils.file(self.username + '_comments.txt')
+
+def followed(self):
+    return utils.file(self.username + '_followed.txt')
+
+def unfollowed(self):
+    return utils.file(self.username + '_unfollowed.txt')
+
+def skipped(self):
+    return utils.file(self.username + '_skipped.txt')
+
+def friends(self):
+    return utils.file(self.username + '_friends.txt')
