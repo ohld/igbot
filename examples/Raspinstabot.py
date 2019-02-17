290fcaf9470d2606ea6fe9c28ad3bfec8d5dc7a6
khairiazim <khairiazim@users.noreply.github.com>
2019-02-17 19:30:31 +0800

    complete v1

--- examples/Raspinstabot.py
+++ examples/Raspinstabot.py
@@ -18,9 +18,21 @@ from itertools import islice
 sys.path.append(os.path.join(sys.path[0], '../'))
 from instabot import Bot
 
-filename = "vetfolname.txt"
+# filename = "vetfolname.txt"
+bot = Bot()
+bot.login(username="bromalayabro", password="subhanallah")
 
-def get_followers_into_file(username,get_what,file_name = filename):
+# required arguments from user
+username = str(input("enter username whose followers or followings you want to get\n")).strip()
+get_what = str(input("followers or followings\n")).strip()
+filename = str(input("enter file name.txt \n")).strip()
+max_follow_unfollow = int(input("max follow unfollow 100 + random value"))
+max_like = int(input("max like 2 + random value"))
+
+
+#######################################################################################################################
+
+def get_followers_into_file():
     try:
         user_id = bot.get_user_id_from_username(username)
     except Exception as e:
@@ -29,45 +41,33 @@ def get_followers_into_file(username,get_what,file_name = filename):
 
     bot.api.get_total_followers_or_followings(user_id=user_id,
                                               which=get_what,
-                                              to_file=file_name)
+                                              to_file=filename,
+                                              usernames=True)
 
-def like_and_follow(max_follow_unfollow,max_like): #file contain username
+def like_and_follow(): #file contain username
     if os.path.getsize(filename):
         with open(filename,"r") as f:
-            for username in islice(f, 0, max_follow_unfollow):
+            for username in islice(f, 0, int(100 + max_follow_unfollow * random.random())):
                 user_id = username.strip()
-                bot.like_user(user_id, amount=max_like)
+                bot.like_user(user_id, amount=int(1 + max_like * random.random()))
                 bot.follow(user_id)
                 time.sleep(10 + 20 * random.random())
+        return True
 
     else:
         print(filename, "empty")
+        return False
 
-def new_list(max_follow_unfollow):
+def new_list():
     with open(filename,"r") as f:
         lines = f.readlines()
-        open(filename, "w").writelines(lines[max_follow_unfollow:])
+        open(filename, "w").writelines(lines[int(100 + max_follow_unfollow * random.random()):])
 
+#######################################################################################################################
 
+get_followers_into_file()
 
-
-
-# bot = Bot()
-# bot.login(username="bromalayabro", password="subhanallah")
-
-# required arguments from user
-# username = str(input("enter username whose followers or followings you want to get")).strip()
-# get_what = str(input("followers or followings")).strip()
-# file_name = str(input("file name")).strip()
-max_follow_unfollow = int(100 + int(input ("max follow unfollow 100 + random value"))* random.random())
-max_like = int(2 + int(input ("max like 2 + random value"))* random.random())
-
-
-# get_followers_into_file(username,get_what,file_name)
-# like_and_follow()
-# new_list()
-# bot.unfollow_non_followers(n_to_unfollows=value)
-
-#while True:
-    # loop all task
-    #pass
+while like_and_follow():
+    like_and_follow()
+    new_list()
+    bot.unfollow_non_followers(n_to_unfollows=max_follow_unfollow)
