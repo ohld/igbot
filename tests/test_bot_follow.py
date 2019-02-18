850e50d1dfd73be867dfac5a43da0cf1e35020ed
lexan73 <18335313+lexan73@users.noreply.github.com>
2019-02-18 16:39:16 +0400

    Update test_bot_follow.py

--- tests/test_bot_follow.py
+++ tests/test_bot_follow.py
@@ -15,11 +15,11 @@ from .test_variables import (TEST_SEARCH_USERNAME_ITEM, TEST_USERNAME_INFO_ITEM,
 
 
 def reset_files(_bot):
-    for x in _bot.followed_file(_bot.self).list:
+    for x in _bot.followed_file.list:
         _bot.followed_file.remove(x)
-    for x in _bot.unfollowed_file(_bot.self).list:
+    for x in _bot.unfollowed_file.list:
         _bot.unfollowed_file.remove(x)
-    for x in _bot.skipped_file(_bot.self).list:
+    for x in _bot.skipped_file.list:
         _bot.skipped_file.remove(x)
 
 
