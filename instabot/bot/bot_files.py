c3de650ef2d55c50d88212f22769139123069ce9
lexan73 <18335313+lexan73@users.noreply.github.com>
2019-02-18 14:56:31 +0400

    Update bot_files.py

--- instabot/bot/bot_files.py
+++ instabot/bot/bot_files.py
@@ -1,22 +1,50 @@
 from .. import utils
 
-def whitelist(self):
-    return utils.file(self.username + '_whitelist.txt')
 
-def blacklist(self):
-    return utils.file(self.username + '_blacklist.txt')
+def set_whitelist(self):
+    if self.default_files:
+        return utils.file('whitelist.txt')
+    else:
+        return utils.file(self.username + '_whitelist.txt')
 
-def comments(self):
-    return utils.file(self.username + '_comments.txt')
 
-def followed(self):
-    return utils.file(self.username + '_followed.txt')
+def set_blacklist(self):
+    if self.default_files:
+        return utils.file('blacklist.txt')
+    else:
+        return utils.file(self.username + '_blacklist.txt')
 
-def unfollowed(self):
-    return utils.file(self.username + '_unfollowed.txt')
 
-def skipped(self):
-    return utils.file(self.username + '_skipped.txt')
+def set_comments(self):
+    if self.default_files:
+        return utils.file('comments.txt')
+    else:
+        return utils.file(self.username + '_comments.txt')
 
-def friends(self):
-    return utils.file(self.username + '_friends.txt')
+
+def set_followed(self):
+    if self.default_files:
+        return utils.file('followed.txt')
+    else:
+        return utils.file(self.username + '_followed.txt')
+
+
+def set_unfollowed(self):
+    if self.default_files:
+        return utils.file('unfollowed.txt')
+    else:
+        return utils.file(self.username + '_unfollowed.txt')
+
+
+def set_skipped(self):
+    if self.default_files:
+        return utils.file('skipped.txt')
+    else:
+        return utils.file(self.username + '_skipped.txt')
+
+
+def set_friends(self):
+    if self.default_files:
+        return utils.file('friends.txt')
+    else:
+        return utils.file(self.username + '_friends.txt')
