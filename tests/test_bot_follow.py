d65b8d2546ca81d9eac9b2683f331a2b4cfb52f3
lexan73 <18335313+lexan73@users.noreply.github.com>
2019-02-18 19:24:54 +0400

    Update test_bot_follow.py

--- tests/test_bot_follow.py
+++ tests/test_bot_follow.py
@@ -25,7 +25,6 @@ def reset_files(_bot):
 
 class TestBotFilter(TestBot):
     
-    default_files = True
 
     @responses.activate
     @pytest.mark.parametrize('username', [TEST_SEARCH_USERNAME_ITEM['username'],
@@ -33,9 +32,6 @@ class TestBotFilter(TestBot):
                                           str(TEST_SEARCH_USERNAME_ITEM['pk'])])
     @patch('time.sleep', return_value=None)
     def test_follow(self, patched_time_sleep, username):
-        self.followed_file = self.bot.followed_file(self)
-        self.unfollowed_file = self.bot.unfollowed_file(self)
-        self.skipped_file = self.bot.skipped_file(self)
         follows_at_start = self.bot.total['follows']
         self.bot._following = [1]
         reset_files(self.bot)
