93cb24cb0c08013ef002fafca507d2643de39e77
khairiazim <khairiazim@users.noreply.github.com>
2019-03-17 20:22:40 +0800

    add workthread

--- examples/vetogram.py
+++ examples/vetogram.py
@@ -1,7 +1,16 @@
-import sys
 import os
+import random
+import shutil
+import sys
+import time
+import threading
+import errno
+import queue
+import requests
+import webbrowser
+import schedule
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtCore,QtGui, QtWidgets
 from PyQt5 import uic
 
 sys.path.append(os.path.join(sys.path[0], '../'))
@@ -17,16 +26,39 @@ class MainWindow_class(QtWidgets.QMainWindow):
         # CLICK RUN BUTTON TRIGGER LOGIN FUNCTION
         self.pushButton_run.clicked.connect(self.login_instagram)
 
+        # PASS UI OBJECT NAME TO WORKTHREAD CLASS
+        self.workThread = workThread(lineEdit_follow_from_hashtag=self.lineEdit_follow_from_hashtag)
+
     def login_instagram(self):
         global bot
         bot = Bot()
         username = str(self.lineEdit_username.text()).lower().strip()
         password = str(self.lineEdit_password.text()).strip()
         if bot.login(username=username, password=password) == 1:
-            pass
+            #ALL TASK START HERE AFTER LOGIN
+            self.workThread.start()
+            #pass
         else:
             QtWidgets.QMessageBox.warning(self, "Ooopps", "wrong username or password")
 
+#MAKE THREAD SO THAT UI DIDNT FREEZE
+class workThread(QtCore.QThread):
+    my_signal = QtCore.pyqtSignal()
+    def __init__(self,lineEdit_follow_from_hashtag,parent=None):
+        super(workThread,self).__init__(parent)
+        # IMPORT UI OBJECT NAME FROM MAINWINDOW CLASS
+        self.lineEdit_follow_from_hashtag = lineEdit_follow_from_hashtag
+
+    def follow_from_hastags(self):
+        hashtags = str(self.lineEdit_follow_from_hashtag.text()).strip().split(",")
+        for hashtag in hashtags:
+            print("Begin following: " + hashtag)
+            users = bot.get_hashtag_users(hashtag)
+            bot.follow_users(users)
+
+    #ALL FUNCTION IN WORKTHREAD START HERE
+    def run(self):
+        self.follow_from_hastags()
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
