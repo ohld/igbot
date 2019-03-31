0c3df911ef6f808a3311ce0d58fae661c7619156
Sherali Karimov <sherali.karimov@gmail.com>
2019-03-31 19:43:02 +0200

    added interactive two-factor authentication support

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -184,10 +184,14 @@ class API(object):
                 return False
         else:
             self.logger.error("Request returns {} error!".format(response.status_code))
-            response_data = json.loads(response.text)
-            if "feedback_required" in str(response_data.get('message')):
-                self.logger.error("ATTENTION!: `feedback_required`, your action could have been blocked")
-                return "feedback_required"
+            try:
+                response_data = json.loads(response.text)
+                if "feedback_required" in str(response_data.get('message')):
+                    self.logger.error("ATTENTION!: `feedback_required`, your action could have been blocked")
+                    return "feedback_required"
+            except ValueError:
+                self.logger.error("Error checking for `feedback_required`, response text is not JSON")
+
             if response.status_code == 429:
                 sleep_minutes = 5
                 self.logger.warning(
@@ -196,6 +200,37 @@ class API(object):
                 time.sleep(sleep_minutes * 60)
             elif response.status_code == 400:
                 response_data = json.loads(response.text)
+
+                # PERFORM Interactive Two-Factor Authentication
+                if response_data.get('two_factor_required'):
+                    self.logger.error("Two-factor authentication required")
+                    two_factor_code = input("Enter 2FA verification code: ")
+                    two_factor_id = response_data['two_factor_info']['two_factor_identifier']
+
+                    login = self.session.post(config.API_URL +'accounts/two_factor_login/',
+                                data={'username': self.username, 
+                                        'verification_code': two_factor_code, 
+                                        'two_factor_identifier': two_factor_id,
+                                        'password': self.password,
+                                        'device_id': self.device_id,
+                                        'ig_sig_key_version':4
+                                        },
+                                allow_redirects=True)
+                    
+                    if login.status_code == 200:
+                        resp_json = json.loads(login.text)
+                        if resp_json['status'] != 'ok':
+                            if 'message' in resp_json:
+                                self.logger.error("Login error: {}".format(resp_json['message']))
+                            else:
+                                self.logger.error("Login error: \"{}\" status and message {}.".format(resp_json['status'], login.text))
+                            return False
+                        return True
+                    else:
+                        self.logger.error("Two-factor authentication request returns {} error with message {} !".format(login.status_code, login.text))
+                        return False
+                # End of Interactive Two-Factor Authentication
+                else:
                 msg = "Instagram's error message: {}"
                 self.logger.info(msg.format(response_data.get('message')))
                 if 'error_type' in response_data:
