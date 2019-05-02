148efeda4919e1ab727c5378bdf7a502e7187353
Travis CI User <travis@example.org>
2019-05-02 22:36:42 +0000

    Change version: 0.35.0 -> 0.36.0 [ci skip]

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='instabot',
-    version='0.35.0',
+    version='0.36.0',
 
     description='Instagram bot scripts for promotion and API python wrapper.',
     long_description=long_description,
