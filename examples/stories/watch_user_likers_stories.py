45991deed8c0dc9c03deb75ea62b94f9f4f8e8bf
Steffan Jensen <6hourapp@gmail.com>
2019-06-25 01:51:21 +0000

    logs to log file

--- examples/stories/watch_user_likers_stories.py
+++ examples/stories/watch_user_likers_stories.py
@@ -24,14 +24,14 @@ bot = Bot()
 bot.login()
 
 if len(sys.argv) >= 2:
-    print(
+    bot.logger.info(
         """
             Going to get '%s' likers and watch their stories (and stories of their likers too).
         """ % (sys.argv[1])
     )
     user_to_get_likers_of = bot.convert_to_user_id(sys.argv[1])
 else:
-    print(
+    bot.logger.info(
         """
             Going to get your likers and watch their stories (and stories of their likers too).
             You can specify username of another user to start (by default we use you as a starting point).
@@ -60,18 +60,18 @@ while True:
 
         # WATCH USERS STORIES
         if bot.watch_users_reels(liker_ids):
-            print("Total stories viewed: %d" % bot.total["stories_viewed"])
+            bot.logger.info("Total stories viewed: %d" % bot.total["stories_viewed"])
 
         # CHOOSE RANDOM LIKER TO GRAB HIS LIKERS AND REPEAT
         current_user_id = random.choice(liker_ids)
 
         if random.random() < 0.05:
             current_user_id = user_to_get_likers_of
-            print("Sleeping and returning back to original user_id=%s" % current_user_id)
+            bot.logger.info("Sleeping and returning back to original user_id=%s" % current_user_id)
             time.sleep(90 * random.random() + 60)
 
     except Exception as e:
         # If something went wrong - sleep long and start again
-        print("Exception:", str(e))
+        bot.logger.info(e)
         current_user_id = user_to_get_likers_of
         time.sleep(240 * random.random() + 60)
