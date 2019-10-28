66bafeacc1f43c8024a7fa254166b5cd76a65b32
theparcel <theparcel@users.noreply.github.com>
2019-10-28 18:29:41 +0100

    Space issue fixed

--- instabot/api/api.py
+++ instabot/api/api.py
@@ -71,9 +71,8 @@ class API(object):
         if not os.path.exists("./config/"):
             os.makedirs("./config/")  # create base_path if not exists
             
-            
         if not os.path.exists("./log/"):
-            os.makedirs("./log/")  # create log path if not exists
+            os.makedirs("./log/")  # create log folder if not exists
 
         if save_logfile is True:
             if log_filename is None:
