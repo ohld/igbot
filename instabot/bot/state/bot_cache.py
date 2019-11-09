296d78ac29ffd6c30b32ba05e330947d5665ec8e
gioelecrispo <crispogioele@gmail.com>
2019-11-09 22:22:30 +0100

    Added BotState and BotCache singleton. They are intended to be shared between more instabot instances to solve the concurrency read/write problem in api.last_json when more tasks are run in threading mode.

---
+++ instabot/bot/state/bot_cache.py
@@ -0,0 +1,13 @@
+from instabot.singleton import Singleton
+
+
+class BotCache(object, metaclass=Singleton):
+
+    def __init__(self):
+        self.following = None
+        self.followers = None
+        self.user_infos = {}  # User info cache
+        self.usernames = {}  # `username` to `user_id` mapping
+
+    def __repr__(self):
+        return self.__dict__
