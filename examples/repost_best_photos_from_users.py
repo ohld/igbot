56799a93b9372e372c523b4f925a536e6290e82a
bruvv <3063928+bruvv@users.noreply.github.com>
2020-01-02 22:11:40 +0100

    Continu even without captions just make it empty

--- examples/repost_best_photos_from_users.py
+++ examples/repost_best_photos_from_users.py
@@ -81,8 +81,12 @@ def repost_photo(bot, new_media_id, path=POSTED_MEDIAS):
         with open(photo_path[:-3] + "txt", "r") as f:
             text = "".join(f.readlines())
     except FileNotFoundError:
-        with open(photo_path[:-6] + ".txt", "r") as f:
-            text = "".join(f.readlines())
+        try:
+            with open(photo_path[:-6] + ".txt", "r") as f:
+                text = "".join(f.readlines())
+        except FileNotFoundError:
+            text = ""
+            pass
     if bot.upload_photo(photo_path, text):
         update_posted_medias(new_media_id, path)
         bot.logger.info("Media_id {} is saved in {}".format(new_media_id, path))
