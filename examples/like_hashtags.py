620ee76dceed54a456d3eae42ba4a6edcf33a537
the predator <7414055@GMAIL.COM>
2020-02-29 10:44:20 -0800

    Update like_hashtags.py

--- examples/like_hashtags.py
+++ examples/like_hashtags.py
@@ -13,8 +13,8 @@ sys.path.append(os.path.join(sys.path[0], "../"))
 from instabot import Bot  # noqa: E402
 
 parser = argparse.ArgumentParser(add_help=True)
-parser.add_argument("-u", type=str, help="username")
-parser.add_argument("-p", type=str, help="password")
+parser.add_argument("-u", type=str, help="twinny._girls_")
+parser.add_argument("-p", type=str, help="ERROR#DK7")
 parser.add_argument("-proxy", type=str, help="proxy")
 parser.add_argument("hashtags", type=str, nargs="+", help="hashtags")
 args = parser.parse_args()
@@ -24,3 +24,5 @@ bot.login(username=args.u, password=args.p, proxy=args.proxy)
 
 for hashtag in args.hashtags:
     bot.like_hashtag(hashtag)
+
+
